9aa9ebc8c7177e9ab8702223eb3c5c9d39e6663b
Howard19960920 <hower.song@gmail.com>
2018-06-15 01:05:15 +0800

    add Q9 palindromes

---
+++ Q9/sampleCode9.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#define MAXLEN 1024
+
+
+int main()
+{
+    int slen, flag;
+    char buff[MAXLEN], *p, *q;
+    while(fgets(buff, MAXLEN, stdin) != NULL)
+    {
+        flag = 1;
+        slen = strlen(buff) - 1;
+        buff[slen] = buff[slen] == '\n' ? '\0' : buff[slen];
+        for(p = &buff[0], q = &buff[strlen(buff) - 1]; q >= p; p++, q--)
+        {
+            if(*p != *q)
+            {
+                flag = 0;
+            }
+        }
+        if(flag)
+        {
+            printf("%s\n", buff);
+        }
+    }
+    return 0;
+}
